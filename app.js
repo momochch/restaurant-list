cf4e28da2eb88294808e4839d236778158aff040
momochch <momo120824@gmail.com>
2024-07-27 23:03:45 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+//用require把restaurant.json傳到樣板引擎中
+const restaurants = require('./public/jsons/restaurant.json').results
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
@@ -12,8 +14,9 @@ app.get('/', (req, res) =>{
   res.redirect('/restaurants')
 })
 
+//main.hbs裡面的{{{body}}}載入的是index.hbs
 app.get('/restaurants', (req, res)=>{
-  res.render('index')
+  res.render('index', { restaurants })
 })
 
 app.get('/restaurant/:id', (req, res)=>{
